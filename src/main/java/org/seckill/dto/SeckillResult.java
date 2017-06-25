8a4f816d15f7e7e5516e74c51a607933740cd0f0
Pumpkin <fyyfyy7764@live.com>
2017-06-25 17:29:36 +0800

    add result java file between viewController and pages

---
+++ src/main/java/org/seckill/dto/SeckillResult.java
@@ -0,0 +1,49 @@
+package org.seckill.dto;
+
+/**
+ * Created by fyy on 6/25/17.
+ */
+//用来封装json结果,所有ajax的请求返回
+public class SeckillResult<T> {
+
+    private boolean success;
+    private T data;
+    private String error;
+
+    //如果success == true 那就要传数据
+    public SeckillResult(boolean success, T data) {
+        this.success = success;
+        this.data = data;
+    }
+
+    //success == false 传递错误信息
+    public SeckillResult(boolean success, String error) {
+
+        this.success = success;
+        this.error = error;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
